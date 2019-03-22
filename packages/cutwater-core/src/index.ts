1fc2623b407f7ca0535cd25b866afd96c08c9912
William Weiss <william@codification.org>
2019-03-21 20:32:26 -0500

    refactor: remove node only class

--- packages/cutwater-core/src/index.ts
+++ packages/cutwater-core/src/index.ts
@@ -2,5 +2,4 @@ export { Env } from './Env';
 export { Config } from './Config';
 export { TimeUnit } from './TimeUnit';
 export { TZUtils } from './TZUtils';
-export { IOUtils } from './IOUtils';
 export { StringUtils } from './StringUtils';
