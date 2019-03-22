0e14adf4be8cf388d631431e851c98d68846416f
William Weiss <william@codification.org>
2019-03-22 08:55:44 -0500

    feat: add VarUtils export

--- packages/cutwater-core/src/index.ts
+++ packages/cutwater-core/src/index.ts
@@ -3,3 +3,4 @@ export { Config } from './Config';
 export { TimeUnit } from './TimeUnit';
 export { TZUtils } from './TZUtils';
 export { StringUtils } from './StringUtils';
+export { VarUtils } from './VarUtils';
