fb922523a1f231754be011cc86ef0be1db29389c
William Weiss <william@codification.org>
2019-03-29 11:20:51 -0500

    test: improve test organization

--- packages/cutwater-node-core/src/HttpUtils.spec.ts
+++ packages/cutwater-node-core/src/HttpUtils.spec.ts
@@ -7,31 +7,33 @@ import { HttpUtils } from './HttpUtils';
 const GOOGLE_URL: string = 'https://www.google.com';
 
 describe('HttpUtils Unit Tests', () => {
-  test('toBodyText', done => {
-    const req: ClientRequest = HttpRequest(GOOGLE_URL, (response: IncomingMessage) => {
-      expect(HttpUtils.isResponseOk(response)).toBeTruthy();
-      HttpUtils.toBodyText(response).then(html => {
-        expect(html).toMatch(/<html/);
-        done();
+  describe('toBodyText', () => {
+    it('correctly returns html content from a node http request', done => {
+      const req: ClientRequest = HttpRequest(GOOGLE_URL, (response: IncomingMessage) => {
+        expect(HttpUtils.isResponseOk(response)).toBeTruthy();
+        HttpUtils.toBodyText(response).then(html => {
+          expect(html).toMatch(/<html/);
+          done();
+        });
+      });
+      req.end();
+    });
+
+    it('correctly returns html content from a got request', done => {
+      got(GOOGLE_URL).then(response => {
+        HttpUtils.toBodyText(response).then(html => {
+          expect(html).toMatch(/<html/);
+          done();
+        });
       });
     });
-    req.end();
-  });
 
-  it('correctly returns html content from a got request', done => {
-    got(GOOGLE_URL).then(response => {
-      HttpUtils.toBodyText(response).then(html => {
-        expect(html).toMatch(/<html/);
-        done();
-      });
-    });
-  });
-
-  it('correctly returns html content from a got buffer response', done => {
-    got(GOOGLE_URL, { encoding: undefined }).then(response => {
-      HttpUtils.toBodyText(response).then(html => {
-        expect(html).toMatch(/<html/);
-        done();
+    it('correctly returns html content from a got buffer response', done => {
+      got(GOOGLE_URL, { encoding: undefined }).then(response => {
+        HttpUtils.toBodyText(response).then(html => {
+          expect(html).toMatch(/<html/);
+          done();
+        });
       });
     });
   });
