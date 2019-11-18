f00416b9a71a193fc02d7ce881f1fc4ed6415219
William Weiss <william@codification.org>
2019-11-18 18:52:03 -0500

    fix: ensure we create types and sourcemaps

--- packages/cutwater-build-typescript/src/tasks/TscTask.ts
+++ packages/cutwater-build-typescript/src/tasks/TscTask.ts
@@ -1,21 +1,52 @@
 import { GulpTask } from '@codification/cutwater-build-core';
 import * as gulp from 'gulp';
+import * as sourcemaps from 'gulp-sourcemaps';
 import * as ts from 'gulp-typescript';
+import { ICompileStream, Project } from 'gulp-typescript/release/project';
+import * as merge from 'merge-stream';
+import { Readable } from 'stream';
 
 export class TscTask extends GulpTask<any> {
   public constructor() {
     super('tsc', {});
   }
 
-  public executeTask(localGulp: gulp.Gulp): NodeJS.ReadWriteStream {
-    const tsProject = ts.createProject('tsconfig.json', this.customArgs());
-    const tsResult = localGulp.src(`${this.buildConfig.srcFolder}/**/*.ts`).pipe(tsProject());
-    return tsResult.js.pipe(localGulp.dest(this.outputFolder()));
+  public executeTask(localGulp: gulp.Gulp): NodeJS.ReadWriteStream | void {
+    const tsProject: Project = ts.createProject('tsconfig.json', this.customArgs());
+
+    let baseStream: any = localGulp.src(`${this.buildConfig.srcFolder}/**/*.ts`);
+    if (this.sourceMapsEnabled(tsProject)) {
+      baseStream = baseStream.pipe(sourcemaps.init());
+    }
+    const compiledStream: ICompileStream = baseStream.pipe(tsProject());
+
+    const streams: Readable[] = [compiledStream.js];
+    if (this.declarationsEnabled(tsProject)) {
+      streams.push(compiledStream.dts);
+    }
+
+    let rval: NodeJS.ReadWriteStream = merge(...streams);
+    if (this.sourceMapsEnabled(tsProject)) {
+      rval = rval.pipe(sourcemaps.write('.'));
+    }
+    return rval.pipe(localGulp.dest(this.outputFolder()));
+  }
+
+  private sourceMapsEnabled(project: Project): boolean {
+    return (
+      project.options !== undefined && project.options.sourceMap !== undefined && project.options.sourceMap === true
+    );
+  }
+
+  private declarationsEnabled(project: Project): boolean {
+    return (
+      project.options !== undefined && project.options.declaration !== undefined && project.options.declaration === true
+    );
   }
 
   private outputFolder(): string {
     const args: object = this.customArgs();
-    // tslint:disable-next-line:no-string-literal
+    // tslint:disable-next-line: no-string-literal
     return args['outDir'] ? args['outDir'] : this.buildConfig.libFolder;
   }
 
