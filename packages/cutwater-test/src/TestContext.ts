4d7a16a3fd330b0e5bc2fc878a6bcd7b322bf19c
William Weiss <william@codification.org>
2020-02-18 22:03:34 -0500

    feat: add initial work on new test utilities

---
+++ packages/cutwater-test/src/TestContext.ts
@@ -0,0 +1,50 @@
+import { FileUtils } from '@codification/cutwater-node-core';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class TestContext {
+  public static createContext(tempDirName?: string): TestContext {
+    return new TestContext(tempDirName);
+  }
+
+  public readonly tempDir: string;
+  private destroyed: boolean = false;
+
+  private constructor(tempDirName: string = 'temp/test') {
+    const tempPath = path.resolve(path.join(process.cwd(), tempDirName)) + path.sep;
+    this.tempDir = FileUtils.createTempDir(tempPath);
+  }
+
+  public createTempFilePath(ext: string = 'tmp'): string {
+    this.checkForDestroyed();
+    return FileUtils.createTempFilePath(this.tempDir, ext);
+  }
+
+  public teardown(): Promise<void> {
+    this.checkForDestroyed();
+    this.destroyed = true;
+    return new Promise((resolve, reject) => {
+      fs.readdir(this.tempDir, (err, files) => {
+        if (!err) {
+          try {
+            files.forEach(file => {
+              fs.unlinkSync(path.join(this.tempDir, file));
+            });
+            fs.rmdirSync(this.tempDir);
+            resolve();
+          } catch (removalErr) {
+            reject(removalErr);
+          }
+        } else {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  private checkForDestroyed(): void {
+    if (this.destroyed) {
+      throw new Error('TestContext has already been torn down.');
+    }
+  }
+}
---
+++ packages/cutwater-test/src/TestUtils.test.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { TestContext } from './TestContext';
+
+describe('TestUtils', () => {
+  describe('getTempDir()', () => {
+    it('can provide a default temp directory', () => {
+      const tempDir = TestContext.createContext().tempDir;
+      expect(tempDir.startsWith(path.resolve('temp/test'))).toBeTruthy();
+      expect(fs.existsSync(tempDir)).toBeTruthy();
+    });
+
+    it('can provide a custom package rooted temp directory', () => {
+      const tempDir = TestContext.createContext('temp/foo').tempDir;
+      expect(tempDir.startsWith(path.resolve('temp/foo'))).toBeTruthy();
+      expect(fs.existsSync(tempDir)).toBeTruthy();
+    });
+  });
+
+  it('can create a temp file path', () => {
+    const ctx = TestContext.createContext();
+    let tempPath = ctx.createTempFilePath();
+    expect(tempPath.endsWith('.tmp')).toBeTruthy();
+    expect(fs.existsSync(tempPath)).toBeFalsy();
+
+    tempPath = ctx.createTempFilePath('jpg');
+    expect(tempPath.endsWith('.jpg')).toBeTruthy();
+  });
+
+  it('can create unique temp file paths', () => {
+    const ctx = TestContext.createContext();
+    const history: string[] = [];
+    for (let i = 0; i < 100; i++) {
+      const tempPath = ctx.createTempFilePath();
+      expect(history.indexOf(tempPath)).toEqual(-1);
+      history.push(tempPath);
+    }
+  });
+
+  it('can remove the temp directory and all contents', async () => {
+    const ctx = TestContext.createContext();
+    for (let i = 0; i < 4; i++) {
+      const tempFile = ctx.createTempFilePath();
+      fs.writeFileSync(tempFile, `just test data ${i}`);
+      expect(fs.existsSync(tempFile)).toBeTruthy();
+      expect(path.dirname(tempFile)).toEqual(ctx.tempDir);
+    }
+    await ctx.teardown();
+    expect(fs.existsSync(ctx.tempDir)).toBeFalsy();
+  });
+});
