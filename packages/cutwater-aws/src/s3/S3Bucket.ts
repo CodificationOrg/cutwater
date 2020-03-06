9481cdd50912a08111e95d011fd18fca9eeba7c9
William Weiss <william@codification.org>
2020-03-06 10:32:51 -0500

    refactor: use AWS promise api

--- packages/cutwater-aws/src/s3/S3Bucket.ts
+++ packages/cutwater-aws/src/s3/S3Bucket.ts
@@ -1,11 +1,5 @@
 import { S3 } from 'aws-sdk';
-import {
-  DeleteObjectRequest,
-  GetObjectOutput,
-  GetObjectRequest,
-  PutObjectOutput,
-  PutObjectRequest,
-} from 'aws-sdk/clients/s3';
+import { GetObjectOutput, GetObjectRequest, PutObjectOutput, PutObjectRequest } from 'aws-sdk/clients/s3';
 import * as mime from 'mime';
 import { Readable } from 'stream';
 
@@ -32,29 +26,21 @@ export class S3Bucket {
     this.bucketName = name;
   }
 
-  public exists(fileName: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      this.s3Client.headObject({ Bucket: this.bucketName, Key: fileName }, (err, data) => {
-        let rval: boolean = true;
-        if (err && err.code === 'NotFound') {
-          rval = false;
-        } else if (err) {
-          return reject(err);
-        }
-        resolve(rval);
-      });
-    });
+  public async exists(fileName: string): Promise<boolean> {
+    let rval: boolean = false;
+    try {
+      await this.s3Client.headObject({ Bucket: this.bucketName, Key: fileName }).promise();
+      rval = true;
+    } catch (err) {
+      if (err.code !== 'NotFound') {
+        throw err;
+      }
+    }
+    return rval;
   }
 
-  public remove(fileName: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.s3Client.deleteObject(this.toDeleteObjectRequest(fileName), (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
+  public async remove(fileName: string): Promise<void> {
+    await this.s3Client.deleteObject(this.toBaseObjectRequest(fileName)).promise();
   }
 
   public async loadBuffer(fileName: string): Promise<Buffer> {
@@ -65,51 +51,31 @@ export class S3Bucket {
     return output.Body;
   }
 
-  public load(fileName: string): Promise<GetObjectOutput> {
-    return new Promise((resolve, reject) => {
-      this.s3Client.getObject(this.toGetObjectRequest(fileName), (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data);
-      });
-    });
+  public async load(fileName: string): Promise<GetObjectOutput> {
+    return await this.s3Client.getObject(this.toBaseObjectRequest(fileName)).promise();
   }
 
-  public store(fileName: string, content: string | Buffer | Readable, mimeType?: string): Promise<PutObjectOutput> {
-    return new Promise((resolve, reject) => {
-      this.s3Client.putObject(this.toPutObjectRequest(fileName, content, mimeType), (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data);
-      });
-    });
+  public async store(
+    fileName: string,
+    content: string | Buffer | Readable,
+    mimeType?: string,
+  ): Promise<PutObjectOutput> {
+    return await this.s3Client.putObject(this.toPutObjectRequest(fileName, content, mimeType)).promise();
   }
 
   private toPutObjectRequest(key: string, content: string | Buffer | Readable, mimeType?: string): PutObjectRequest {
     const rval: PutObjectRequest = {
+      ...this.toBaseObjectRequest(key),
       Body: content,
-      Bucket: this.bucketName,
       ContentType: mimeType ? mimeType : S3Bucket.toMimeType(key),
-      Key: key,
     };
     return rval;
   }
 
-  private toGetObjectRequest(key: string): GetObjectRequest {
-    const rval: GetObjectRequest = {
+  private toBaseObjectRequest(key: string): GetObjectRequest {
+    return {
       Bucket: this.bucketName,
       Key: key,
     };
-    return rval;
-  }
-
-  private toDeleteObjectRequest(key: string): DeleteObjectRequest {
-    const rval: DeleteObjectRequest = {
-      Bucket: this.bucketName,
-      Key: key,
-    };
-    return rval;
   }
 }
