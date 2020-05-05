8d46c0c3568d3281b42673a7a2f7856cd472289a
William Weiss <william@codification.org>
2020-05-05 16:34:43 -0500

    fix: remove dependence on specific task for config

--- packages/cutwater-build-webpack/src/tasks/webpack.config.ts
+++ packages/cutwater-build-webpack/src/tasks/webpack.config.ts
@@ -1,13 +1,10 @@
-import { IOUtils } from '@codification/cutwater-build-core';
+import { BuildConfig, getConfig, IOUtils } from '@codification/cutwater-build-core';
 import * as path from 'path';
 import * as TerserPlugin from 'terser-webpack-plugin';
 import * as Webpack from 'webpack';
-import * as wpBuild from '../';
-import { WebpackTask } from './WebpackTask';
 
-const webpackTask: WebpackTask = wpBuild.webpack;
-
-const isProduction: boolean = webpackTask.buildConfig.production;
+const buildConfig: BuildConfig = getConfig();
+const isProduction: boolean = buildConfig.production;
 
 const packageJSON: { name: string } = IOUtils.readJSONSyncSafe('./package.json');
 
@@ -17,12 +14,12 @@ const webpackConfiguration: Webpack.Configuration = {
   devtool: isProduction ? undefined : 'source-map',
 
   entry: {
-    [packageJSON.name]: path.join(__dirname, webpackTask.buildConfig.libFolder, 'index.js'),
+    [packageJSON.name]: path.join(__dirname, buildConfig.libFolder, 'index.js'),
   },
 
   output: {
     libraryTarget: 'umd',
-    path: path.join(__dirname, webpackTask.buildConfig.distFolder),
+    path: path.join(__dirname, buildConfig.distFolder),
     filename: `[name]${isProduction ? '.min' : ''}.js`,
     globalObject: 'this',
   },
