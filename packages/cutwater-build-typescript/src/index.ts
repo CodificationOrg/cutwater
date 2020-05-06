634bb5008e2183a882d9940b3501df64f83b6b36
William Weiss <william@codification.org>
2020-05-06 18:49:25 -0500

    refactor: remove task rename

--- packages/cutwater-build-typescript/src/index.ts
+++ packages/cutwater-build-typescript/src/index.ts
@@ -9,7 +9,6 @@ export * from './tasks/TsLintTask';
 const SRC_WATCH_GLOB: string[] = [`${getConfig().srcFolder}/**/*.ts*`, `!${getConfig().srcFolder}/**/*.test.*`];
 
 export const tsc: ExecutableTask = new TscTask();
-tsc.name = 'tsc-commonjs';
 export const tscWatch: ExecutableTask = watch(SRC_WATCH_GLOB, tsc);
 
 export const tscAmd: TscTask = new TscTask();
