3cf71d6a66c776503f981804d04489d456a4dbb6
William Weiss <william@codification.org>
2021-04-19 20:42:01 -0500

    test: add more tests for ItemCache

--- packages/cutwater-graphql/src/repositories/ItemCache.test.ts
+++ packages/cutwater-graphql/src/repositories/ItemCache.test.ts
@@ -2,17 +2,17 @@ import { InMemoryLRUCache } from 'apollo-server-caching';
 import { ItemCache } from './ItemCache';
 import { PropertyDescriptor } from './PropertyDescriptor';
 
-interface MockItem {
-  id: string;
+export interface MockItem {
+  userId: string;
   name: string;
   age: number;
 }
 
-const mockItems = (count: number) => {
+export const mockItems = (count: number) => {
   const rval: MockItem[] = [];
   for (let i = 0; i < count; i++) {
     rval.push({
-      id: `${i}`,
+      userId: `${i}`,
       name: `name${i}`,
       age: i,
     });
@@ -20,13 +20,18 @@ const mockItems = (count: number) => {
   return rval;
 };
 
-const newInstance = () =>
-  new ItemCache<MockItem>(new InMemoryLRUCache(), {
+const newInstance = async (count?: number): Promise<ItemCache<MockItem>> => {
+  const rval = new ItemCache<MockItem>(new InMemoryLRUCache(), {
     repoName: 'stuff',
     cacheId: 'PLACEHOLDER',
-    itemDescriptor: new PropertyDescriptor('id', 'parent'),
+    itemDescriptor: new PropertyDescriptor('userId', 'parent'),
     ttl: 50,
   });
+  if (count) {
+    await rval.putAll(mockItems(count));
+  }
+  return rval;
+};
 
 describe('ItemCache', () => {
   describe('constructor', () => {
@@ -43,14 +48,34 @@ describe('ItemCache', () => {
 
   describe('includes', () => {
     it('can determine if the cache includes an id', async () => {
-      const cache = newInstance();
-      await cache.put(mockItems(1)[0]);
+      const cache = await newInstance(1);
       expect(cache.includes('0')).toBeTruthy();
     });
     it('can determine if the cache does not include an id', async () => {
-      const cache = newInstance();
-      await cache.put(mockItems(1)[0]);
+      const cache = await newInstance(3);
       expect(cache.includes('42')).toBeFalsy();
+    });
+  });
+
+  describe('getAll', () => {
+    it('can get all', async () => {
+      const cache = await newInstance(15);
+      const items = await cache.getAll();
+      expect(items.length).toBe(15);
+    });
+    it('includes items added later', async () => {
+      const cache = await newInstance(15);
+      const newItem = mockItems(1)[0];
+      newItem.userId = '42';
+      await cache.put(newItem);
+      const items = await cache.getAll();
+      expect(items.length).toBe(16);
+    });
+    it('excludes removed items', async () => {
+      const cache = await newInstance(15);
+      await cache.remove('10');
+      const items = await cache.getAll();
+      expect(items.length).toBe(14);
     });
   });
 });
