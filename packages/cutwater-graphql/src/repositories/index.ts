105332c08cd6ec9aea120fa714ef8dff14f63a9c
William Weiss <william@codification.org>
2021-04-27 17:57:01 -0500

    refactor: add missing files to barrel file

--- packages/cutwater-graphql/src/repositories/index.ts
+++ packages/cutwater-graphql/src/repositories/index.ts
@@ -1,4 +1,6 @@
 export * from './CachingItemRepository';
 export * from './ItemDescriptor';
 export * from './ItemRepositoryAdapter';
+export * from './MemoryItemRepository';
 export * from './NodeItemDescriptor';
+export * from './PropertyDescriptor';
