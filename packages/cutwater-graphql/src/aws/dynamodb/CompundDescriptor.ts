18a8344b281138d33f3ac0f8307b4d14c90c24ea
William Weiss <william@codification.org>
2021-09-23 12:03:34 -0500

    feat: add default descriptor for compound repo

---
+++ packages/cutwater-graphql/src/aws/dynamodb/CompundDescriptor.ts
@@ -0,0 +1,30 @@
+import { ItemDescriptor, NodeId, NodeItemDescriptor } from '../..';
+import { CompoundKey } from './CompoundKey';
+
+export class PropertyDescriptor<T> implements ItemDescriptor<T>, NodeItemDescriptor<T> {
+  public constructor(private readonly nodeType, private readonly idProperty: string) {}
+
+  public getId(item: T): string {
+    return item[this.idProperty];
+  }
+
+  public getParentId(item: T): string {
+    return CompoundKey.fromItemId(this.nodeType, this.getId(item)).parentItemId;
+  }
+
+  public getItemId(nodeId: NodeId): string {
+    return nodeId.objectId;
+  }
+
+  public getItemParentId(nodeId: NodeId): string {
+    return CompoundKey.fromNodeId(nodeId).parentItemId;
+  }
+
+  public getObjectId(item: T): string {
+    return this.getId(item);
+  }
+
+  public getParentObjectId(item: T): string {
+    return this.getParentId(item);
+  }
+}
