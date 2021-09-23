c2c15adcc28b5c5362f0a875baf5dd8a9c670d12
William Weiss <william@codification.org>
2021-09-23 18:03:53 -0500

    refactor: remove old prop

--- packages/cutwater-graphql/src/aws/dynamodb/CompoundKeyItemRepositoryConfig.ts
+++ packages/cutwater-graphql/src/aws/dynamodb/CompoundKeyItemRepositoryConfig.ts
@@ -4,6 +4,5 @@ import { CompoundMapper } from './CompoundMapper';
 export interface CompoundKeyItemRepositoryConfig<T> {
   db?: DynamoDB;
   tableName: string;
-  typeIndex: string;
   mapper: CompoundMapper<T>;
 }
