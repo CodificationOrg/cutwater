80e2302fc9a9f546a97d521ce6088174f1303f3b
William Weiss <william@codification.org>
2023-06-01 10:57:36 -0500

    refactor: update aws build package exports

--- packages/cutwater-build-aws/src/index.ts
+++ packages/cutwater-build-aws/src/index.ts
@@ -2,10 +2,10 @@ import { ExecutableTask, serial, task } from '@codification/cutwater-build-core'
 import { buildImageTask, tagAndPushImageTask } from '@codification/cutwater-build-docker';
 import { buildTasks } from '@codification/cutwater-build-node';
 import { openapiBundle } from '@codification/cutwater-build-openapi';
+import { BuildLambdaImageTask } from './tasks/BuildLambdaImageTask';
 import { CloudFormationDeployTask } from './tasks/CloudFormationDeployTask';
 import { CloudFormationPackageTask } from './tasks/CloudFormationPackageTask';
 import { EcrLoginTask } from './tasks/EcrLoginTask';
-import { BuildLambdaImageTask } from './tasks/BuildLambdaImageTask';
 import { S3CopyTask } from './tasks/S3CopyTask';
 import { SamPackageTask } from './tasks/SamPackageTask';
 import { SamPublishTask } from './tasks/SamPublishTask';
@@ -16,13 +16,12 @@ export const samPackageTask: ExecutableTask<unknown> = new SamPackageTask();
 export const samPublishTask: ExecutableTask<unknown> = new SamPublishTask();
 export const cfPackageTask: ExecutableTask<unknown> = new CloudFormationPackageTask();
 export const cfDeployTask: ExecutableTask<unknown> = new CloudFormationDeployTask();
-export const prepareLambdaImageAssetsTask: ExecutableTask<unknown> = new BuildLambdaImageTask();
+export const buildLambdaImageTask: ExecutableTask<unknown> = new BuildLambdaImageTask();
 
 export const openApiCfPackageTasks: ExecutableTask<unknown> = serial(openapiBundle, cfPackageTask);
 export const openApiCfDeployTasks: ExecutableTask<unknown> = serial(openApiCfPackageTasks, cfDeployTask);
 
-export const buildDockerLambdaTask: ExecutableTask<unknown> = serial(buildTasks, prepareLambdaImageAssetsTask);
-export const packageDockerLambdaTask: ExecutableTask<unknown> = serial(buildDockerLambdaTask, buildImageTask);
+export const packageDockerLambdaTask: ExecutableTask<unknown> = serial(buildTasks, buildImageTask);
 export const publishDockerLambdaTask: ExecutableTask<unknown> = serial(
   packageDockerLambdaTask,
   ecrLoginTask,
@@ -31,7 +30,7 @@ export const publishDockerLambdaTask: ExecutableTask<unknown> = serial(
 
 task('s3-copy', s3CopyTask);
 task('ecr-login', ecrLoginTask);
-task('prepare-lambda-image-assets', prepareLambdaImageAssetsTask);
+task('prepare-lambda-image-assets', buildLambdaImageTask);
 task('sam-package', samPackageTask);
 task('sam-publish', samPublishTask);
 task('sam-package-publish', serial(samPackageTask, samPublishTask));
@@ -39,6 +38,6 @@ task('cloudformation-package', cfPackageTask);
 task('cloudformation-deploy', cfDeployTask);
 task('openapi-cf-package', openApiCfPackageTasks);
 task('openapi-cf-deploy', openApiCfDeployTasks);
-task('build-docker-lambda', buildDockerLambdaTask);
+task('build-lambda-image', buildLambdaImageTask);
 task('package-docker-lambda', packageDockerLambdaTask);
 task('publish-docker-lambda', publishDockerLambdaTask);
