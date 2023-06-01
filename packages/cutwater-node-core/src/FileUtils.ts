1784e1e01777d604154ebe05669fa4cb436b106e
William Weiss <william@codification.org>
2023-06-01 10:41:58 -0500

    refactor: improve fs and path imports

--- packages/cutwater-node-core/src/FileUtils.ts
+++ packages/cutwater-node-core/src/FileUtils.ts
@@ -1,35 +1,32 @@
 import { LoggerFactory } from '@codification/cutwater-logging';
-import fs from 'fs';
+import { existsSync, mkdirSync, mkdtempSync, rmdirSync } from 'fs';
 import os from 'os';
-import path from 'path';
+import { dirname, join, resolve } from 'path';
 import uuid from 'uuid/v4';
 
 const LOG = LoggerFactory.getLogger();
 
 export class FileUtils {
-  public static createTempDir(prefix: string = path.join(os.tmpdir(), 'cutwater-'), recursive = true): string {
+  public static createTempDir(prefix: string = join(os.tmpdir(), 'cutwater-'), recursive = true): string {
     if (recursive && !this.doesParentDirectoryExist(prefix)) {
-      fs.mkdirSync(path.resolve(path.dirname(prefix)), { recursive: true });
+      mkdirSync(resolve(dirname(prefix)), { recursive: true });
     }
-    const rval = fs.mkdtempSync(prefix);
+    const rval = mkdtempSync(prefix);
     LOG.debug(`Created temp directory: ${rval}`);
     return rval;
   }
 
   public static doesParentDirectoryExist(filePath: string): boolean {
-    return fs.existsSync(path.dirname(path.resolve(filePath)));
+    return existsSync(dirname(resolve(filePath)));
   }
 
   public static createTempFilePath(tempDir?: string, ext = 'tmp'): string {
-    const rval = path.join(
-      tempDir ? tempDir : this.createTempDir(),
-      `${uuid()}${ext.startsWith('.') ? ext : '.' + ext}`,
-    );
+    const rval = join(tempDir ? tempDir : this.createTempDir(), `${uuid()}${ext.startsWith('.') ? ext : '.' + ext}`);
     LOG.debug(`Created temp file path: ${rval}`);
     return rval;
   }
 
   public static deleteDirectory(dirPath: string, recursive = true, maxRetries = 5, retryDelay = 100): void {
-    fs.rmdirSync(dirPath, { recursive, retryDelay, maxRetries });
+    rmdirSync(dirPath, { recursive, retryDelay, maxRetries });
   }
 }
