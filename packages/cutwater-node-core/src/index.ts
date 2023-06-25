e599f114fb8f03e6c6b8c7df52d53a58c64aa598
William Weiss <william@codification.org>
2023-06-25 10:09:33 -0500

    feat: re-export useful Nullables

--- packages/cutwater-node-core/src/index.ts
+++ packages/cutwater-node-core/src/index.ts
@@ -1,3 +1,5 @@
+export { System } from '@codification/cutwater-build-core';
+export { FileReference } from '@codification/cutwater-build-core/lib/types/FileReference';
 export { FileUtils } from './FileUtils';
 export * from './HttpService';
 export { HttpUtils } from './HttpUtils';
--- packages/cutwater-test/src/index.ts
+++ packages/cutwater-test/src/index.ts
@@ -1,1 +1,4 @@
+export { OutputTracker } from '@codification/cutwater-build-core';
+export { TrackedDuplex } from '@codification/cutwater-build-core/lib/support/TrackedDuplex';
+export { TrackedIOStreams } from '@codification/cutwater-build-core/lib/support/TrackedIOStreams';
 export { TestContext } from './TestContext';
